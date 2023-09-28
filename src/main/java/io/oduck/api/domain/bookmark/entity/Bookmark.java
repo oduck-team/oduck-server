40b0503dcb06f506f480a9fec0da24d586a91dc8
FaberJoo <sjdk0805@gmail.com>
2023-09-28 17:31:43 +0900

    feat: 북마크 엔티티 추가 #3

---
+++ src/main/java/io/oduck/api/domain/bookmark/entity/Bookmark.java
@@ -0,0 +1,30 @@
+package io.oduck.api.domain.bookmark.entity;
+
+import io.oduck.api.domain.anime.entity.Anime;
+import io.oduck.api.domain.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Bookmark {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "anime_id")
+  private Anime anime;
+}
