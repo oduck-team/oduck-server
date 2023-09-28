77491495918902d2cf5e530760c7a3faf1df3c09
FaberJoo <sjdk0805@gmail.com>
2023-09-28 17:31:25 +0900

    feat: 리뷰 및 평가 관련 엔티티 추가 #3

---
+++ src/main/java/io/oduck/api/domain/reviewLike/entity/ShortReviewLike.java
@@ -0,0 +1,31 @@
+package io.oduck.api.domain.reviewLike.entity;
+
+import io.oduck.api.domain.member.entity.Member;
+import io.oduck.api.domain.review.entity.ShortReview;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ShortReviewLike {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "short_review_id")
+  private ShortReview shortReview;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+}
---
+++ src/main/java/io/oduck/api/domain/starRating/entity/StarRating.java
@@ -0,0 +1,34 @@
+package io.oduck.api.domain.starRating.entity;
+
+import io.oduck.api.domain.anime.entity.Anime;
+import io.oduck.api.domain.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class StarRating {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "anime_id")
+  private Anime anime;
+
+  @Column(nullable = false)
+  private int score;
+}
