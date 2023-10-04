074c227afe6a45e1acbbd99876b17cdfefa0c4ef
FaberJoo <sjdk0805@gmail.com>
2023-10-04 15:41:07 +0900

    refactor: member entity 빌더 추가 #10

--- src/main/java/io/oduck/api/domain/member/entity/MemberProfile.java
+++ src/main/java/io/oduck/api/domain/member/entity/MemberProfile.java
@@ -11,13 +11,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
-@Getter
+@Builder
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
 public class MemberProfile extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,18 +35,39 @@ public class MemberProfile extends BaseEntity {
   private String name;
 
   @Column(nullable = false, length = 50)
-  private String info;
+  @Builder.Default
+  private String info = "";
 
   @Enumerated(EnumType.STRING)
-  private Role role;
+  @Builder.Default
+  private Role role = Role.MEMBER;
 
   @Column(length = 100)
-  private String thumbnail;
+  @Builder.Default
+  private String thumbnail = "";
 
   @Column(length = 100)
-  private String backgroundImage;
+  @Builder.Default
+  private String backgroundImage = "";
 
   @Column(nullable = false)
   @ColumnDefault("0")
-  private Long point;
+  @Builder.Default
+  private Long point = 0L;
+
+  @Builder
+  public MemberProfile(Long id, String name, String info, Role role, String thumbnail,
+      String backgroundImage, Long point) {
+    this.id = id;
+    this.name = name;
+    this.info = info;
+    this.role = role;
+    this.thumbnail = thumbnail;
+    this.backgroundImage = backgroundImage;
+    this.point = point;
+  }
+
+  public void setMember(Member member) {
+    this.member = member;
+  }
 }
