07a99f0af368df5dd20085c5e1be3f9242f8a917
FaberJoo <sjdk0805@gmail.com>
2023-10-04 18:20:17 +0900

    refactor: 안쓰는 import 제거 #10

--- src/main/java/io/oduck/api/global/security/handler/LoginSuccessHandler.java
+++ src/main/java/io/oduck/api/global/security/handler/LoginSuccessHandler.java
@@ -5,23 +5,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Value("${config.base.url}")
     private String BASE_URL;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-        Authentication authentication) throws IOException, ServletException {
+            Authentication authentication) throws IOException, ServletException {
         response.sendRedirect(BASE_URL);
     }
 }
