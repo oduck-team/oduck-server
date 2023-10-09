7495fedecd6bc77ffeb5f12b32206b8331b87fb0
FaberJoo <sjdk0805@gmail.com>
2023-10-09 12:03:59 +0900

    refactor: 시큐리티 설정에 로컬 로그인 필터 추가 #13

--- src/main/java/io/oduck/api/global/security/config/SecurityConfig.java
+++ src/main/java/io/oduck/api/global/security/config/SecurityConfig.java
@@ -2,17 +2,20 @@ package io.oduck.api.global.security.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.oduck.api.domain.member.entity.Role;
+import io.oduck.api.global.security.filter.LocalAuthenticationFilter;
 import io.oduck.api.global.security.handler.ForbiddenHandler;
 import io.oduck.api.global.security.handler.LoginFailureHandler;
 import io.oduck.api.global.security.handler.LoginSuccessHandler;
-import io.oduck.api.global.security.auth.service.CustomOAuth2UserService;
+import io.oduck.api.global.security.auth.service.SocialLoginService;
 import io.oduck.api.global.security.handler.LogoutHandler;
 import io.oduck.api.global.security.handler.UnauthorizedHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +23,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
@@ -27,8 +32,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 public class SecurityConfig {
 
+    private final ObjectMapper objectMapper;
     private final LogoutHandler logoutHandler;
-    private final CustomOAuth2UserService customOAuth2UserService;
+    private final SocialLoginService socialLoginService;
     private final LoginSuccessHandler loginSuccessHandler;
     private final LoginFailureHandler loginFailureHandler;
     private final ForbiddenHandler forbiddenHandler;
@@ -84,18 +90,24 @@ public class SecurityConfig {
                     .invalidateHttpSession(true).permitAll(false)
             );
 
+        // Custom Configurer : LocalAuthenticationFilter 를 등록하는 역할
+        http
+            .apply(new CustomFilterConfigurer());
+
         // OAuth2 로그인 설정
         http
             .oauth2Login((oauth2Login) ->
                 oauth2Login
                 .userInfoEndpoint((userInfoEndpoint) ->
                     userInfoEndpoint
-                        .userService(customOAuth2UserService)
+                        .userService(socialLoginService)
                 )
                 .successHandler(loginSuccessHandler)
                 .failureHandler(loginFailureHandler)
             );
 
+        // 예외 처리 설정
+        // 인증 인가 예외 처리
         http
             .exceptionHandling((exceptionHandling) ->
                 exceptionHandling
@@ -111,8 +123,50 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         // Spring Security 에서 지원하는 PasswordEncoder 구현 객체를 생성해주는 컴포넌트.
         // DelegatingPasswordEncoder를 통해 애플리케이션에서 사용할 PasswordEncoder 를 결정하고,
-        // 결정된 PasswordEncoder로 사용자가 입력한 패스워드를 단방향으로 암호화 해준다.
+        // 결정된 PasswordEncoder로 사용자가 입력한 패스워드를 단방향으로 암호화 해줌.
         // 기본 BCrypt
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+//    // Custom Configurer : CustomFilterConfigurer 는 직접 구현한 필터인 JwtAuthenticationFilter 를 등록하는 역할
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        // AbstractHttpConfigurer 를 상속하여 구현
+        // AbstractHttpConfigurer<AbstractHttpConfigurer 를 상속하는 타입, HttpSecurityBuilder 를 상속하는 타입> 을 지정
+
+        // configure() 메서드를 오버라이드해서 Configuration 을 커스터마이징
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+
+            // getSharedObject() 를 통해서 Spring Security 의 설정을 구성하는 SecurityConfigurer 간에 공유되는 객체를 획득가능
+            // getSharedObject(AuthenticationManager.class)를 통해 AuthenticationManager 객체 획득
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            // LocalAuthenticationFilter 생성
+            LocalAuthenticationFilter localAuthenticationFilter = new LocalAuthenticationFilter(authenticationManager, objectMapper);
+
+            // url과 httpMethod 를 통해 요청을 필터링하는데 사용할 RequestMatcher 를 지정
+            localAuthenticationFilter.setRequiresAuthenticationRequestMatcher(
+                new AntPathRequestMatcher("/auth/login", "POST")
+            );
+
+            // 인증에 성공하면 SecurityContext 에 Authentication 객체를 저장하고 로드하는데,
+            // 이러한 역할을 SecurityContextRepository 클래스에서 담당.
+            SecurityContextRepository contextRepository = new HttpSessionSecurityContextRepository();
+            localAuthenticationFilter.setSecurityContextRepository(contextRepository);
+
+            // 추가 처리 Custom Handler 를 필터에 등록
+            localAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler);
+            localAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler);
+
+            // Spring Security Filter Chain에 추가
+            builder
+                .addFilter(localAuthenticationFilter);
+        }
     }
 }
