c718d125e48a0719ec0fbf5c926b0259ab2fddd7
jaykayBaek <jaykaybaek@gmail.com>
2023-10-09 13:01:38 +0900

    test: 애니 테스트에 프로필 설정 추가 #16

--- src/test/java/io/oduck/api/e2e/anime/AnimeControllerTest.java
+++ src/test/java/io/oduck/api/e2e/anime/AnimeControllerTest.java
@@ -22,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -31,6 +32,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
+@ActiveProfiles("test")
 public class AnimeControllerTest {
 
     @Autowired
