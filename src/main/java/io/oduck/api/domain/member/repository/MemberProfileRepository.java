fc86637009d137c34a4c7d5ef088b4b2a8b42678
FaberJoo <sjdk0805@gmail.com>
2023-10-09 11:57:56 +0900

    feat: Member Profile repository 추다 #13

--- src/main/java/io/oduck/api/domain/member/repository/MemberProfileRepository.java
+++ src/main/java/io/oduck/api/domain/member/repository/MemberProfileRepository.java
@@ -1,10 +1,13 @@
 package io.oduck.api.domain.member.repository;
 
 import io.oduck.api.domain.member.entity.MemberProfile;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberProfileRepository extends JpaRepository<MemberProfile,Long> {
 
+    Optional<MemberProfile> findByMemberId(Long memberId);
+    boolean existsByMemberId(Long memberId);
 }
