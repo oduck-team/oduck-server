7e39c6acd2de1070b116fd09b6cbccc2bd9bfdc0
FaberJoo <sjdk0805@gmail.com>
2023-10-10 15:38:16 +0900

    test: 데이터 초기화 위한 DataInitializer 추가 #13

---
+++ src/test/java/io/oduck/api/global/initializer/TestDataInitializer.java
@@ -0,0 +1,49 @@
+package io.oduck.api.global.initializer;
+
+import io.oduck.api.domain.member.entity.LoginType;
+import io.oduck.api.domain.member.entity.Member;
+import io.oduck.api.domain.member.entity.MemberProfile;
+import io.oduck.api.domain.member.entity.Role;
+import io.oduck.api.domain.member.repository.MemberRepository;
+import io.oduck.api.global.security.auth.entity.AuthLocal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
+
+@Component
+@ActiveProfiles("test")
+public class TestDataInitializer {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public TestDataInitializer(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // TODO: Test Data Helper로 분리하기
+    public Member saveTestMember() {
+        Member member = Member.builder()
+            .loginType(LoginType.LOCAL)
+            .role(Role.MEMBER)
+            .build();
+
+        AuthLocal authLocal = AuthLocal.builder()
+            .email("bob@gmail.com")
+            .password("{bcrypt}$2a$10$C0G6uQz.MzfsSH7BZFRBz.MPBmFSV2zAloqqBIwaUpxnmMgCQK..i")
+            .build();
+
+        MemberProfile memberProfile = MemberProfile.builder()
+            .name("admin")
+            .info("bob info")
+            .thumbnail("bob thumbnail")
+            .point(0L)
+            .build();
+
+        member.setAuthLocal(authLocal);
+        member.setMemberProfile(memberProfile);
+
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+}
