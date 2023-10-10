ca8e3cc7d231427627b89b974f3027a6b8bb705f
hanyMK <hany.mi.kim@gmail.com>
2023-10-10 22:14:26 +0900

    test: ShortReviewController, Service Test 및 문서화 #9

---
+++ src/test/java/io/oduck/api/e2e/shortReview/ShortReviewControllerTest.java
@@ -0,0 +1,174 @@
+package io.oduck.api.e2e.shortReview;
+
+import static io.oduck.api.global.config.RestDocsConfig.field;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.attributes;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+
+
+import com.google.gson.Gson;
+import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
+@SpringBootTest
+@ActiveProfiles("test")
+public class ShortReviewControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Gson gson;
+
+    @Nested
+    @DisplayName("짧은 리뷰 조회")
+    class GetShortReviews{
+
+        @DisplayName("조회 성공 시 Http Status 200 반환")
+        @Test
+        void getShortReviews() throws Exception{
+            //given
+            Long animeId = 1L;
+
+            //when
+            ResultActions actions = mockMvc.perform(
+                get("/short-reviews" + "/animeId/{animeId}", animeId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+            );
+
+            //then
+            actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.item.shortReview[0].animeId").exists())
+                .andExpect(jsonPath("$.item.shortReview[0].content").exists())
+                .andExpect(jsonPath("$.item.shortReview[0].hasSpoiler").exists())
+                .andExpect(jsonPath("$.item.shortReview[0].score").exists())
+                .andExpect(jsonPath("$.item.shortReview[0].shortReviewLikeCount").exists())
+                .andExpect(jsonPath("$.item.shortReview[0].member.name").exists())
+                .andExpect(jsonPath("$.item.shortReview[0].member.thumbnail").exists())
+                .andDo(document("getShortReviews/success",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    pathParameters(
+                        parameterWithName("animeId")
+                            .description("애니 아이디")
+                    ),
+                    responseFields(
+                        fieldWithPath("item")
+                            .type(JsonFieldType.OBJECT)
+                            .description("조회 데이터"),
+                        fieldWithPath("item.shortReview")
+                            .type(JsonFieldType.ARRAY)
+                            .description("리뷰 데이터"),
+                        fieldWithPath("item.shortReview[0].animeId")
+                            .type(JsonFieldType.NUMBER)
+                            .description("애니 고유 식별자"),
+                        fieldWithPath("item.shortReview[0].content")
+                            .type(JsonFieldType.STRING)
+                            .description("짧은 리뷰 내용"),
+                        fieldWithPath("item.shortReview[0].hasSpoiler")
+                            .type(JsonFieldType.BOOLEAN)
+                            .description("스포일러 유무"),
+                        fieldWithPath("item.shortReview[0].score")
+                            .type(JsonFieldType.NUMBER)
+                            .description("평점"),
+                        fieldWithPath("item.shortReview[0].shortReviewLikeCount")
+                            .type(JsonFieldType.NUMBER)
+                            .description("리뷰 좋아요 수")
+                        ,fieldWithPath("item.shortReview[0].member")
+                             .type(JsonFieldType.OBJECT)
+                             .description("회원 관련 데이터"),
+                        fieldWithPath("item.shortReview[0].member.name")
+                            .type(JsonFieldType.STRING)
+                            .description("회원 이름"),
+                        fieldWithPath("item.shortReview[0].member.thumbnail")
+                            .type(JsonFieldType.STRING)
+                            .description("회원 이미지 사진")
+                    )
+                ));
+            //TODO : 조회 실패 시
+        }
+    }
+
+
+//    @Nested
+//    @DisplayName("짧은 리뷰 작성")
+//    class PostShortReviews{
+//
+//        @DisplayName(("리뷰 작성 성공시 200 ok 반환"))
+//        @Test
+//        void postShortReview() throws Exception{
+//            //given
+//            PostShortReviewReq req = PostShortReviewReq
+//                                         .builder()
+//                                         .memberId(2L)
+//                                         .animeId(1L)
+//                                         .content("최고의 반전 올해 최고의 애니!!!")
+//                                         .hasSpoiler(false)
+//                                         .build();
+//
+//            String content = gson.toJson(req);
+//
+//            //when
+//            ResultActions actions = mockMvc.perform(
+//                post("/short-reviews")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .accept(MediaType.APPLICATION_JSON)
+//                    .content(content));
+//
+//            //then
+//            actions
+//                .andExpect(status().isOk())
+//                .andDo(document("postShortReview/success",
+//                    preprocessRequest(prettyPrint()),
+//                    preprocessResponse(prettyPrint()),
+//                    requestFields(attributes(key("title").value("Fields for shortReview creation")),
+//                            fieldWithPath("memberId")
+//                                .type(JsonFieldType.NUMBER)
+//                                .description("리뷰를 등록할 회원의 고유 식별 번호"),
+//                            fieldWithPath("animeId")
+//                                 .type(JsonFieldType.NUMBER)
+//                                 .description("리뷰를 등록할 애니 고유 식별 번호"),
+//                            fieldWithPath("content")
+//                                .type(JsonFieldType.STRING)
+//                                .attributes(
+//                                        field("constraints", "최소 10에서 100자 까지 입력 가능합니다."))
+//                                .description("짧은 리뷰 내용"),
+//                            fieldWithPath("hasSpoiler")
+//                                .type(JsonFieldType.BOOLEAN)
+//                                .description("스포일러 유무"))
+//                    )
+//                );
+//        }
+//        //TODO: 리뷰 작성 실패 시
+//    }
+}
---
+++ src/test/java/io/oduck/api/unit/shortReview/service/ShortReviewServiceTest.java
@@ -0,0 +1,45 @@
+package io.oduck.api.unit.shortReview.service;
+
+import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
+import io.oduck.api.domain.review.dto.ShortReviewResDto;
+import io.oduck.api.domain.review.service.ShortReviewServiceStub;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class ShortReviewServiceTest {
+
+    @InjectMocks
+    private ShortReviewServiceStub shortReviewService;
+
+    @Nested
+    @DisplayName("짧은 리뷰 조회")
+    class GetShortReviews{
+
+        @Test
+        @DisplayName("짧은 리뷰 조회")
+        void getShortReviews(){
+            //given
+            Long animeId = 1L;
+
+            //when
+            ShortReviewResDto response = shortReviewService.getShortReviews(animeId);
+
+            //then
+            assertDoesNotThrow(() -> shortReviewService.getShortReviews(animeId));
+            assertEquals(response.getShortReview().get(0).getAnimeId(),animeId);
+        }
+    }
+
+
+}
