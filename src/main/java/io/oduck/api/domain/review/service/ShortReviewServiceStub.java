b9823de47acbc380c9117386eba18d2c6ab6344b
hanyMK <hany.mi.kim@gmail.com>
2023-10-10 22:08:34 +0900

    feat: ShortReviewServiceStub 추가 #9

---
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceStub.java
@@ -0,0 +1,43 @@
+package io.oduck.api.domain.review.service;
+
+import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
+import io.oduck.api.domain.review.dto.ShortReviewResDto;
+import io.oduck.api.domain.review.dto.ShortReviewResDto.MemberProfile;
+import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReview;
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShortReviewServiceStub implements ShortReviewService{
+
+    @Override
+    public ShortReviewResDto getShortReviews(Long animeId) {
+        ArrayList<ShortReview> list = new ArrayList<>();
+        ShortReview shortReview =  createReview(animeId);
+        list.add(shortReview);
+        return ShortReviewResDto
+                   .builder()
+                   .shortReview(list)
+                   .build();
+    }
+
+    private ShortReview createReview(Long animeId){
+
+        return ShortReview
+                   .builder()
+                   .animeId(1L)
+                   .content("최고의 반전의 반전")
+                   .score(5)
+                   .hasSpoiler(false)
+                   .shortReviewLikeCount(100)
+                   .member(getMemberProfile())
+                   .build();
+    }
+
+    private MemberProfile getMemberProfile(){
+        return MemberProfile.builder()
+                   .name("오덕12")
+                   .thumbnail("사진 url")
+                   .build();
+    }
+}
