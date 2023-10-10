1643ec1cfd96a613876a6d6110fe48131d44affc
FaberJoo <sjdk0805@gmail.com>
2023-10-10 16:20:04 +0900

    test: authController 수정 #13
    - ReaqurestFields 추가
    - requestHeader 추가

--- src/test/java/io/oduck/api/e2e/auth/AuthControllerTest.java
+++ src/test/java/io/oduck/api/e2e/auth/AuthControllerTest.java
@@ -1,29 +1,41 @@
 package io.oduck.api.e2e.auth;
 
+import static io.oduck.api.global.config.RestDocsConfig.field;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.snippet.Attributes.attributes;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.google.gson.Gson;
+import io.oduck.api.domain.member.entity.LoginType;
 import io.oduck.api.domain.member.entity.Role;
 import io.oduck.api.global.initializer.TestDataInitializer;
 import io.oduck.api.global.MockMember.WithCustomMockMember;
+import io.oduck.api.global.security.auth.dto.AuthUser;
 import io.oduck.api.global.security.auth.dto.LocalAuthDto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
@@ -43,6 +55,9 @@ public class AuthControllerTest {
 
     @Autowired
     private Gson gson;
+
+    @Mock
+    MockHttpSession httpsession;
 
     private final String BASE_URL = "/auth";
 
@@ -80,7 +95,25 @@ public class AuthControllerTest {
                 .andDo(
                     document("postLogin/success",
                         preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                            attributes(key("title")
+                                .value("Fields for member creation")),
+                            fieldWithPath("email")
+                                .type(JsonFieldType.STRING)
+                                .attributes(field("constraints", "이메일 형식"))
+                                .description("로그인에 필요한 이메일"),
+                            fieldWithPath("password")
+                                .type(JsonFieldType.STRING)
+                                .attributes(field("constraints",
+                                    "숫자, 영문대소문자, 특수문자(!@#$%^&*()-_=+)를 포함한 8~20자리"))
+                                .description("로그인에 필요한 비밀 번호")),
+                        responseHeaders(
+                            headerWithName(HttpHeaders.LOCATION) // 헤더 이름
+                                .description("Header Location, 리소스의 URL"),
+                            headerWithName(HttpHeaders.SET_COOKIE) // 헤더 이름
+                                .description("Header Set-Cookie, 세션 쿠키")
+                        )
                     )
                 );
         }
@@ -97,9 +130,11 @@ public class AuthControllerTest {
             // given
 
             // when
-            ResultActions actions = mockMvc.perform(get(BASE_URL + "/status")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
+            ResultActions actions = mockMvc.perform(
+                get(BASE_URL + "/status")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.COOKIE, "oDuckio.sid={SESSION_VALUE}")
             );
 
             // then
@@ -108,6 +143,11 @@ public class AuthControllerTest {
                         document("getStatus/success",
                             preprocessRequest(prettyPrint()),
                             preprocessResponse(prettyPrint()),
+                            requestHeaders(
+                                headerWithName(HttpHeaders.COOKIE)
+                                    .attributes(field("constraints", "oDuckio.sid={SESSION_VALUE}"))
+                                    .description("Header Cookie, 세션 쿠키")
+                            ),
                             responseFields(
                                 fieldWithPath("item")
                                     .type(JsonFieldType.OBJECT)
