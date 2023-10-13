71cd6032ccfeaa3983c9db8f5ec4fc43d9f96212
hanyMK <hany.mi.kim@gmail.com>
2023-10-13 12:18:50 +0900

    refactor: GetMapping주소 /animeId/{animeId} ->/{animeId}변경, SingleResponse -> SliceResponse변경  #9

--- src/main/java/io/oduck/api/domain/review/controller/ShortReviewController.java
+++ src/main/java/io/oduck/api/domain/review/controller/ShortReviewController.java
@@ -2,7 +2,7 @@ package io.oduck.api.domain.review.controller;
 
 import io.oduck.api.domain.review.dto.ShortReviewResDto;
 import io.oduck.api.domain.review.service.ShortReviewService;
-import io.oduck.api.global.common.SingleResponse;
+import io.oduck.api.global.common.SliceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,11 +21,11 @@ public class ShortReviewController {
     private final ShortReviewService shortReviewService;
 
     //애니의 짧은 리뷰 조회
-    @GetMapping("/animeId/{animeId}")
+    @GetMapping("/{animeId}")
     public ResponseEntity<?> getShortReviews(
         @PathVariable Long animeId)  {
         //TODO: 애니에 따른 짧은 리뷰 조회
         ShortReviewResDto reviewResDto = shortReviewService.getShortReviews(animeId);
-        return ResponseEntity.ok(SingleResponse.of(reviewResDto));
+        return ResponseEntity.ok(SliceResponse.of(reviewResDto.getShortReviews()));
     }
 }
