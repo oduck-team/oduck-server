f48df2ded1398d8c9ce58ad62712873be7ac9eae
FaberJoo <sjdk0805@gmail.com>
2023-10-15 17:10:54 +0900

    test: 테스트 스텁 데이터 분리 #22

--- src/test/java/io/oduck/api/global/initializer/TestDataInitializer.java
+++ src/test/java/io/oduck/api/global/initializer/TestDataInitializer.java
@@ -1,11 +1,7 @@
 package io.oduck.api.global.initializer;
 
-import io.oduck.api.domain.member.entity.LoginType;
-import io.oduck.api.domain.member.entity.Member;
-import io.oduck.api.domain.member.entity.MemberProfile;
-import io.oduck.api.domain.member.entity.Role;
 import io.oduck.api.domain.member.repository.MemberRepository;
-import io.oduck.api.global.security.auth.entity.AuthLocal;
+import io.oduck.api.global.stub.MemberStub;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,27 +20,9 @@ public class TestDataInitializer {
 
     @PostConstruct
     public void saveTestMember() {
-        // TODO: Test Data들 Helper로 분리하기
-        Member member = Member.builder()
-            .loginType(LoginType.LOCAL)
-            .role(Role.MEMBER)
-            .build();
-
-        AuthLocal authLocal = AuthLocal.builder()
-            .email("bob@gmail.com")
-            .password("{bcrypt}$2a$10$C0G6uQz.MzfsSH7BZFRBz.MPBmFSV2zAloqqBIwaUpxnmMgCQK..i")
-            .build();
-
-        MemberProfile memberProfile = MemberProfile.builder()
-            .name("admin")
-            .info("bob info")
-            .thumbnail("bob thumbnail")
-            .point(0L)
-            .build();
-
-        member.setAuthLocal(authLocal);
-        member.setMemberProfile(memberProfile);
-
-        memberRepository.save(member);
+        // 회원 데이터 초기화
+        MemberStub memberstub = new MemberStub();
+        memberstub.init();
+        memberRepository.saveAll(memberstub.getMembers());
     }
 }
---
+++ src/test/java/io/oduck/api/global/stub/MemberStub.java
@@ -0,0 +1,84 @@
+package io.oduck.api.global.stub;
+
+import io.oduck.api.domain.member.entity.LoginType;
+import io.oduck.api.domain.member.entity.Member;
+import io.oduck.api.domain.member.entity.MemberProfile;
+import io.oduck.api.domain.member.entity.Role;
+import io.oduck.api.global.security.auth.entity.AuthLocal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberStub {
+    List<Member> members = new ArrayList<>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void init() {
+        // Member1
+        Member member1 = Member.builder()
+            .loginType(LoginType.LOCAL)
+            .role(Role.MEMBER)
+            .build();
+
+        AuthLocal authLocal1 = AuthLocal.builder()
+            .email("oduckdmin@gmail.com")
+            .password("{bcrypt}$2a$10$C0G6uQz.MzfsSH7BZFRBz.MPBmFSV2zAloqqBIwaUpxnmMgCQK..i")
+            .build();
+
+        MemberProfile memberProfile1 = MemberProfile.builder()
+            .name("admin")
+            .info("admin info")
+            .thumbnail("admin thumbnail")
+            .point(0L)
+            .build();
+
+        member1.setAuthLocal(authLocal1);
+        member1.setMemberProfile(memberProfile1);
+
+        // member2
+        Member member2 = Member.builder()
+            .loginType(LoginType.LOCAL)
+            .role(Role.MEMBER)
+            .build();
+
+        AuthLocal authLocal2 = AuthLocal.builder()
+            .email("john@gmail.com")
+            .password("{bcrypt}$2a$10$C0G6uQz.MzfsSH7BZFRBz.MPBmFSV2zAloqqBIwaUpxnmMgCQK..i")
+            .build();
+
+        MemberProfile memberProfile2 = MemberProfile.builder()
+            .name("john")
+            .info("john info")
+            .thumbnail("john thumbnail")
+            .point(0L)
+            .build();
+
+        member2.setAuthLocal(authLocal2);
+        member2.setMemberProfile(memberProfile2);
+
+        // member3
+        Member member3 = Member.builder()
+            .loginType(LoginType.LOCAL)
+            .role(Role.MEMBER)
+            .build();
+
+        AuthLocal authLocal3 = AuthLocal.builder()
+            .email("david@gmail.com")
+            .password("{bcrypt}$2a$10$C0G6uQz.MzfsSH7BZFRBz.MPBmFSV2zAloqqBIwaUpxnmMgCQK..i")
+            .build();
+
+        MemberProfile memberProfile3 = MemberProfile.builder()
+            .name("david")
+            .info("david info")
+            .thumbnail("david thumbnail")
+            .point(0L)
+            .build();
+
+        member3.setAuthLocal(authLocal3);
+        member3.setMemberProfile(memberProfile3);
+
+        members.addAll(List.of(member1, member2, member3));
+    }
+}
