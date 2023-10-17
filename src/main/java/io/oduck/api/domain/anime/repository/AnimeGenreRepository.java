c214ee9382b4b21015ea17ebe329415790f76416
jaykayBaek <jaykaybaek@gmail.com>
2023-10-18 00:04:07 +0900

    feat: animeId로 애니 연결 테이블 List 찾아오는 메소드 추가 #28

--- src/main/java/io/oduck/api/domain/anime/repository/AnimeGenreRepository.java
+++ src/main/java/io/oduck/api/domain/anime/repository/AnimeGenreRepository.java
@@ -1,8 +1,16 @@
 package io.oduck.api.domain.anime.repository;
 
 import io.oduck.api.domain.anime.entity.AnimeGenre;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnimeGenreRepository extends JpaRepository<AnimeGenre, Long> {
 
+    @Query("select ag from AnimeGenre ag "
+        + "join ag.anime "
+        + "join ag.genre "
+        + "where ag.anime.id = :animeId")
+    List<AnimeGenre> findAllByAnimeId(@Param("animeId") Long animeId);
 }
--- src/main/java/io/oduck/api/domain/anime/repository/AnimeOriginalAuthorRepository.java
+++ src/main/java/io/oduck/api/domain/anime/repository/AnimeOriginalAuthorRepository.java
@@ -1,8 +1,16 @@
 package io.oduck.api.domain.anime.repository;
 
 import io.oduck.api.domain.anime.entity.AnimeOriginalAuthor;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnimeOriginalAuthorRepository extends JpaRepository<AnimeOriginalAuthor, Long> {
 
+    @Query("select aoa from AnimeOriginalAuthor aoa "
+        + "join fetch aoa.anime "
+        + "join fetch aoa.originalAuthor "
+        + "where aoa.anime.id = :animeId")
+    List<AnimeOriginalAuthor> findAllByAnimeId(@Param("animeId") Long animeId);
 }
