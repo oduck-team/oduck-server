e87c65d0ee453f046a5d2a13a037fb4058291543
FaberJoo <sjdk0805@gmail.com>
2023-10-18 17:33:41 +0900

    feat: Bookmark Dto 추가 #22

---
+++ src/main/java/io/oduck/api/domain/bookmark/dto/BookmarkResDto.java
@@ -0,0 +1,32 @@
+package io.oduck.api.domain.bookmark.dto;
+
+import io.oduck.api.domain.bookmark.dto.BookmarkDslDto.BookmarkDsl;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class BookmarkResDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BookmarkRes {
+        private Long animeId;
+        private String title;
+        private String thumbnail;
+        private Integer myScore;
+        private LocalDateTime createdAt;
+
+        public static BookmarkRes of(BookmarkDsl bookmarkDsl) {
+            return BookmarkRes.builder()
+                .animeId(bookmarkDsl.getAnimeId())
+                .title(bookmarkDsl.getTitle())
+                .thumbnail(bookmarkDsl.getThumbnail())
+                .myScore(bookmarkDsl.getMyScore())
+                .createdAt(bookmarkDsl.getCreatedAt())
+                .build();
+        }
+    }
+}
