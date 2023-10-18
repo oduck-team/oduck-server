9f16e7b6e283614198e235c414a4e166b9eb2a0a
FaberJoo <sjdk0805@gmail.com>
2023-10-18 17:30:50 +0900

    test: MemberInitializer -> MemberContruct #22

--- src/test/java/io/oduck/api/global/initializer/MemberConstruct.java
+++ src/test/java/io/oduck/api/global/initializer/MemberConstruct.java
@@ -2,6 +2,7 @@ package io.oduck.api.global.initializer;
 
 import io.oduck.api.domain.member.repository.MemberRepository;
 import io.oduck.api.global.stub.MemberStub;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -10,13 +11,13 @@ import org.springframework.test.context.ActiveProfiles;
 
 @Component
 @ActiveProfiles("test")
-public class MemberInitializer implements ApplicationRunner {
+public class MemberConstruct {
 
     @Autowired
     MemberRepository memberRepository;
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
+    @PostConstruct
+    public void run() {
         MemberStub memberStub = new MemberStub();
         memberRepository.saveAll(memberStub.getMembers());
     }
