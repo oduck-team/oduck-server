0b33407391abcb0cac61e14338610a4afa8c2bbd
FaberJoo <sjdk0805@gmail.com>
2023-10-19 17:46:10 +0900

    refactor: bookmark res dto에 EntityBased 구현 추가 #22

--- src/main/java/io/oduck/api/domain/bookmark/dto/BookmarkResDto.java
+++ src/main/java/io/oduck/api/domain/bookmark/dto/BookmarkResDto.java
@@ -1,6 +1,7 @@
 package io.oduck.api.domain.bookmark.dto;
 
 import io.oduck.api.domain.bookmark.dto.BookmarkDslDto.BookmarkDsl;
+import io.oduck.api.global.common.EntityBased;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +13,7 @@ public class BookmarkResDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class BookmarkRes {
+    public static class BookmarkRes implements EntityBased {
         private Long animeId;
         private String title;
         private String thumbnail;
@@ -27,6 +28,11 @@ public class BookmarkResDto {
                 .myScore(bookmarkDsl.getMyScore())
                 .createdAt(bookmarkDsl.getCreatedAt())
                 .build();
+        }
+
+        @Override
+        public Long getId() {
+            return this.animeId;
         }
     }
 }
