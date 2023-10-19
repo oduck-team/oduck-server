8827191f2fa68ea4ea1ccea6517dd84df0aff88c
FaberJoo <sjdk0805@gmail.com>
2023-10-19 18:14:54 +0900

    refactor: SliceResponse 수정 #22

--- src/main/java/io/oduck/api/global/common/SliceResponse.java
+++ src/main/java/io/oduck/api/global/common/SliceResponse.java
@@ -8,20 +8,37 @@ import org.springframework.data.domain.Slice;
 public class SliceResponse<T extends EntityBased> {
     private final List<T> items;
     private final int size;             // 한 페이지에 보여줄 아이템의 개수
-    private final boolean isLastPage;   // 마지막 페이지일 경우, true 반환.
+    private final boolean hasNext;   // 마지막 페이지일 경우, true 반환.
     private Long lastId;                // 마지막 아이템의 id
+
     public SliceResponse(Slice<T> sliceContent) {
         this.items = sliceContent.getContent();
         this.size = sliceContent.getSize();
-        this.isLastPage = sliceContent.isLast();
+        this.hasNext = sliceContent.isLast();
 
-        if (!items.isEmpty() && !isLastPage) {
+        if (!items.isEmpty() && !hasNext) {
             T lastItem = items.get(items.size() - 1);
             lastId = lastItem.getId();
         }
     }
 
-    public static <T extends EntityBased> SliceResponse<T> of(Slice<T> sliceContent) {
-        return new SliceResponse<>(sliceContent);
+    public SliceResponse(Slice<?> slice, List<T> items) {
+        this.items = items;
+        this.size = slice.getSize();
+        this.hasNext = slice.hasNext();
+
+        if (!items.isEmpty() && hasNext) {
+            T lastItem = items.get(items.size() - 1);
+            long lastId = lastItem.getId();
+            this.lastId = lastId;
+        }
+    }
+
+    public static <T extends EntityBased> SliceResponse<T> of(Slice<T> slice) {
+        return new SliceResponse<>(slice);
+    }
+
+    public static <T extends EntityBased> SliceResponse<T> of(Slice<?> slice, List<T> items) {
+        return new SliceResponse<>(slice, items);
     }
 }
