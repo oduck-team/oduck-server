64a9e4d05eb0bd903ad10038ed9774e1f4a0a57e
hanyMK <hany.mi.kim@gmail.com>
2023-10-22 20:44:40 +0900

    feat: ShortReviewServiceImpl 추가 및 stub  주석처리  #33

---
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
@@ -0,0 +1,100 @@
+package io.oduck.api.domain.review.service;
+
+import io.oduck.api.domain.anime.entity.Anime;
+import io.oduck.api.domain.anime.repository.AnimeRepository;
+import io.oduck.api.domain.member.entity.Member;
+import io.oduck.api.domain.member.repository.MemberRepository;
+import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
+import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
+import io.oduck.api.domain.review.dto.ShortReviewResDto;
+import io.oduck.api.domain.review.entity.ShortReview;
+import io.oduck.api.domain.review.repository.ShortReviewRepository;
+import io.oduck.api.global.exception.BadRequestException;
+import io.oduck.api.global.exception.NotFoundException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ShortReviewServiceImpl implements ShortReviewService{
+
+    private final ShortReviewRepository shortReviewRepository;
+    private final MemberRepository memberRepository;
+    private final AnimeRepository animeRepository;
+
+    @Override
+    public ShortReviewResDto getShortReviews(Long anime) {
+        return null;
+    }
+
+    @Override
+    public void save(Long memberId, PostShortReviewReq shortReviewReq) {
+        ShortReview shortReview = ShortReview
+                                      .builder()
+                                      .content(shortReviewReq.getContent())
+                                      .hasSpoiler(shortReviewReq.isHasSpoiler())
+                                      .build();
+
+        //애니 입력
+        shortReview.relateAnime(getAnime(shortReviewReq.getAnimeId()));
+
+        //회원 입력
+        shortReview.relateMember(getMember(memberId));
+
+        ShortReview saveShortReview = shortReviewRepository.save(shortReview);
+        log.info("ShortReview Crated! {}", saveShortReview.getId());
+    }
+
+    @Override
+    public void update(Long reviewId, PatchShortReviewReq req) {
+        ShortReview findShortReview = getShortReview(reviewId);
+
+        Optional
+            .ofNullable(req.getContent())
+                .ifPresent(
+                    content ->{
+                        if(findShortReview.getContent().equals(req.getContent())){
+                            throw new BadRequestException("Same content as before.");
+                        }
+                        findShortReview.updateContent(req.getContent());
+                    }
+                );
+
+        Optional
+            .ofNullable(req.getContent())
+            .ifPresent(
+                hasSpoiler ->{
+                    if(findShortReview.isHasSpoiler() == req.isHasSpoiler()){
+                        throw new BadRequestException("Same spoiler as before.");
+                    }
+                    findShortReview.updateSpoiler(req.isHasSpoiler());
+                }
+            );
+        shortReviewRepository.save(findShortReview);
+    }
+
+    private Member getMember(Long memberId){
+
+        return memberRepository.findById(memberId)
+                   .orElseThrow(
+                       () -> new NotFoundException("Member")
+                   );
+    }
+
+    private Anime getAnime(Long animeId){
+        return animeRepository.findById(animeId)
+                   .orElseThrow(
+                       () -> new NotFoundException("Anime")
+                   );
+    }
+
+    private ShortReview getShortReview(Long reviewId){
+        return shortReviewRepository.findById(reviewId)
+                   .orElseThrow(
+                       () -> new NotFoundException("shortReview")
+                   );
+    }
+}
--- src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceStub.java
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceStub.java
@@ -1,66 +1,66 @@
-package io.oduck.api.domain.review.service;
-
-import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
-import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
-import io.oduck.api.domain.review.dto.ShortReviewResDto;
-import io.oduck.api.domain.review.dto.ShortReviewResDto.MemberProfile;
-import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReview;
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ShortReviewServiceStub implements ShortReviewService{
-
-    @Override
-    public ShortReviewResDto getShortReviews(Long animeId) {
-        List<ShortReview> shortReviewList = new ArrayList<>();
-
-        for(int i = 0; i < 10; i++){
-            ShortReview shortReview =  createReview(animeId);
-            shortReviewList.add(shortReview);
-        }
-        //slice객체 만들기
-        PageRequest pageable = PageRequest.of(0,10);
-        Slice<ShortReview> slice = new SliceImpl<>(shortReviewList,pageable,true);
-
-        return ShortReviewResDto
-                   .builder()
-                   .shortReviews(slice)
-                   .build();
-    }
-
-    @Override
-    public void save(PostShortReviewReq shortReviewReq) {
-
-    }
-
-    @Override
-    public void update(Long reviewId, PatchShortReviewReq req) {
-
-    }
-
-    private ShortReview createReview(Long animeId){
-
-        return ShortReview
-                   .builder()
-                   .reviewId(1L)
-                   .animeId(1L)
-                   .content("최고의 반전의 반전")
-                   .score(5)
-                   .hasSpoiler(true)
-                   .shortReviewLikeCount(100)
-                   .member(getMemberProfile())
-                   .build();
-    }
-
-    private MemberProfile getMemberProfile(){
-        return MemberProfile.builder()
-                   .name("오덕12")
-                   .thumbnail("사진 url")
-                   .build();
-    }
-}
+//package io.oduck.api.domain.review.service;
+//
+//import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
+//import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
+//import io.oduck.api.domain.review.dto.ShortReviewResDto;
+//import io.oduck.api.domain.review.dto.ShortReviewResDto.MemberProfile;
+//import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReview;
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Slice;
+//import org.springframework.data.domain.SliceImpl;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ShortReviewServiceStub implements ShortReviewService{
+//
+//    @Override
+//    public ShortReviewResDto getShortReviews(Long animeId) {
+//        List<ShortReview> shortReviewList = new ArrayList<>();
+//
+//        for(int i = 0; i < 10; i++){
+//            ShortReview shortReview =  createReview(animeId);
+//            shortReviewList.add(shortReview);
+//        }
+//        //slice객체 만들기
+//        PageRequest pageable = PageRequest.of(0,10);
+//        Slice<ShortReview> slice = new SliceImpl<>(shortReviewList,pageable,true);
+//
+//        return ShortReviewResDto
+//                   .builder()
+//                   .shortReviews(slice)
+//                   .build();
+//    }
+//
+//    @Override
+//    public void save(PostShortReviewReq shortReviewReq) {
+//
+//    }
+//
+//    @Override
+//    public void update(Long reviewId, PatchShortReviewReq req) {
+//
+//    }
+//
+//    private ShortReview createReview(Long animeId){
+//
+//        return ShortReview
+//                   .builder()
+//                   .reviewId(1L)
+//                   .animeId(1L)
+//                   .content("최고의 반전의 반전")
+//                   .score(5)
+//                   .hasSpoiler(true)
+//                   .shortReviewLikeCount(100)
+//                   .member(getMemberProfile())
+//                   .build();
+//    }
+//
+//    private MemberProfile getMemberProfile(){
+//        return MemberProfile.builder()
+//                   .name("오덕12")
+//                   .thumbnail("사진 url")
+//                   .build();
+//    }
+//}
