f3fb7d94406f92aa2694237c7991bce5b9b0ba7d
jaykayBaek <jaykaybaek@gmail.com>
2023-10-23 16:40:57 +0900

    feat: 스튜디오 등록, 응답 서비스 로직 추가 #39

--- src/main/java/io/oduck/api/domain/studio/service/StudioService.java
+++ src/main/java/io/oduck/api/domain/studio/service/StudioService.java
@@ -1,4 +1,13 @@
 package io.oduck.api.domain.studio.service;
 
+import io.oduck.api.domain.studio.dto.StudioRes;
+
+import java.util.List;
+
+import static io.oduck.api.domain.studio.dto.StudioReq.PostReq;
+
 public interface StudioService {
+    void save(PostReq req);
+
+    List<StudioRes> getStudios();
 }
