e456086112a5655d93589c0f812cd1458c7559fe
jaykayBaek <jaykaybaek@gmail.com>
2023-10-23 16:41:36 +0900

    feat: 장르 등록, 응답 dto 추가 #39

---
+++ src/main/java/io/oduck/api/domain/genre/dto/GenreRes.java
@@ -0,0 +1,11 @@
+package io.oduck.api.domain.genre.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GenreRes {
+    private Long id;
+    private String name;
+}
