2c978ea1da12f620331242a22f1e390ca7770365
jaykayBaek <jaykaybaek@gmail.com>
2023-10-24 11:51:44 +0900

    fix: Valid 어노테이션 수정 #38

--- src/main/java/io/oduck/api/domain/anime/dto/AnimeReq.java
+++ src/main/java/io/oduck/api/domain/anime/dto/AnimeReq.java
@@ -6,6 +6,7 @@ import io.oduck.api.domain.anime.entity.Rating;
 import io.oduck.api.domain.anime.entity.Status;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,33 +20,34 @@ public class AnimeReq {
     @Getter
     public static class PostReq {
         @NotBlank
-        @Length(min = 1, max = 50,
-            message = "글자 수는 0~50을 허용합니다.")
+        @Length(min = 1, max = 50, message = "글자 수는 0~50을 허용합니다.")
         private String title;
 
         @NotBlank
-        @Length(min = 1, max = 255,
-            message = "글자 수는 0~255를 허용합니다.")
+        @Length(min = 1, max = 255, message = "글자 수는 0~255를 허용합니다.")
         private String summary;
 
+        @NotNull
         private BroadcastType broadcastType;
 
-        @Min(value = 0,
-            message = "음수가 올 수 없습니다.")
+        @NotNull(message = "에피소드 수를 입력하세요.")
+        @Min(value = 0, message = "음수가 올 수 없습니다.")
         private int episodeCount;
 
-        @NotBlank(
-            message = "섬네일을 입력하세요.")
+        @NotBlank(message = "섬네일을 입력하세요.")
         private String thumbnail;
 
-        @Min(value = 1900,
-            message = "1900년대 이상을 입력하세요.")
+        @NotNull(message = "년도를 입력하세요.")
+        @Min(value = 1900, message = "1900년대 이상을 입력하세요.")
         private int year;
 
+        @NotNull(message = "분기를 입력하세요.")
         private Quarter quarter;
 
+        @NotNull(message = "심의를 입력하세요.")
         private Rating rating;
 
+        @NotNull(message = "방영 상태를 입력하세요.")
         private Status status;
 
         private List<Long> originalAuthorIds;
@@ -105,33 +107,34 @@ public class AnimeReq {
     @AllArgsConstructor
     public static class PatchAnimeReq{
         @NotBlank
-        @Length(min = 1, max = 50,
-            message = "글자 수는 0~50을 허용합니다.")
+        @Length(min = 1, max = 50, message = "글자 수는 0~50을 허용합니다.")
         private String title;
 
         @NotBlank
-        @Length(min = 1, max = 255,
-            message = "글자 수는 0~255를 허용합니다.")
+        @Length(min = 1, max = 255, message = "글자 수는 0~255를 허용합니다.")
         private String summary;
 
+        @NotNull
         private BroadcastType broadcastType;
 
-        @Min(value = 0,
-            message = "음수가 올 수 없습니다.")
+        @NotNull(message = "에피소드 수를 입력하세요.")
+        @Min(value = 0, message = "음수가 올 수 없습니다.")
         private int episodeCount;
 
-        @NotBlank(
-            message = "섬네일을 입력하세요.")
+        @NotBlank(message = "섬네일을 입력하세요.")
         private String thumbnail;
 
-        @Min(value = 1900,
-            message = "1900년대 이상을 입력하세요.")
+        @NotNull(message = "년도를 입력하세요.")
+        @Min(value = 1900, message = "1900년대 이상을 입력하세요.")
         private int year;
 
+        @NotNull(message = "분기를 입력하세요.")
         private Quarter quarter;
 
+        @NotNull(message = "심의를 입력하세요.")
         private Rating rating;
 
+        @NotNull(message = "방영 상태를 입력하세요.")
         private Status status;
     }
 
