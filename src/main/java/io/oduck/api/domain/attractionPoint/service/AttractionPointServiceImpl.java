af08ff68be73db98ddacab1a6539714b1f83057d
hanyMK <hany.mi.kim@gmail.com>
2023-10-27 17:47:38 +0900

    feat: impl 추가 #33

---
+++ src/main/java/io/oduck/api/domain/attractionPoint/service/AttractionPointServiceImpl.java
@@ -0,0 +1,46 @@
+package io.oduck.api.domain.attractionPoint.service;
+
+import io.oduck.api.domain.attractionPoint.dto.AttractionPointResDto.IsAttractionPoint;
+import io.oduck.api.domain.attractionPoint.entity.AttractionPoint;
+import io.oduck.api.domain.attractionPoint.repository.AttractionElementRepository;
+import io.oduck.api.domain.attractionPoint.repository.AttractionPointRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AttractionPointServiceImpl implements AttractionPointService {
+
+    private final AttractionPointRepository attractionPointRepository;
+    private final AttractionElementRepository attractionElementRepository;
+    @Override
+    public IsAttractionPoint isAttractionPoint(Long memberId, Long animeId) {
+        boolean drawing = false;
+        boolean story = false;
+        boolean music = false;
+        boolean character = false;
+        boolean voiceActor = false;
+
+        List<AttractionPoint> points = attractionPointRepository.findAllByAnimeId_memberId(memberId, animeId);
+        for (AttractionPoint point : points) {
+            switch (point.getAttractionElement()) {
+                case DRAWING -> drawing = true;
+                case MUSIC -> music = true;
+                case STORY -> story = true;
+                case CHARACTER -> character = true;
+                default -> voiceActor = true;
+            }
+        }
+        return IsAttractionPoint
+                      .builder()
+                      .drawing(drawing)
+                      .story(story)
+                      .music(music)
+                      .character(character)
+                      .voiceActor(voiceActor)
+                      .build();
+    }
+}
