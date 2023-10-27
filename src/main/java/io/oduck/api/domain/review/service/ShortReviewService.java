b8804a41e879e3e40a88265f1f40bc82eef17574
hanyMK <hany.mi.kim@gmail.com>
2023-10-27 17:43:35 +0900

    feat: 리뷰 조회 반환값, 변수 변경, impl 수정 #33

--- src/main/java/io/oduck/api/domain/review/service/ShortReviewService.java
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewService.java
@@ -2,12 +2,15 @@ package io.oduck.api.domain.review.service;
 
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
-import io.oduck.api.domain.review.dto.ShortReviewResDto;
+import io.oduck.api.domain.review.dto.ShortReviewReqDto.Sort;
+import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReviewRes;
+import io.oduck.api.global.common.OrderDirection;
+import io.oduck.api.global.common.SliceResponse;
 
 public interface ShortReviewService {
 
     //애니 짧은 리뷰 조회
-    ShortReviewResDto getShortReviews(Long anime);
+    SliceResponse<ShortReviewRes> getShortReviews(Long animeId, String cursor, Sort sort, OrderDirection order, int size);
 
     //애니 리뷰 작성
     void save(Long memberId, PostShortReviewReq shortReviewReq);
--- src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
@@ -1,19 +1,28 @@
 package io.oduck.api.domain.review.service;
 
+import static io.oduck.api.global.utils.PagingUtils.applyPageableForNonOffset;
+
 import io.oduck.api.domain.anime.entity.Anime;
 import io.oduck.api.domain.anime.repository.AnimeRepository;
 import io.oduck.api.domain.member.entity.Member;
+import io.oduck.api.domain.member.repository.MemberProfileRepository;
 import io.oduck.api.domain.member.repository.MemberRepository;
+import io.oduck.api.domain.review.dto.ShortReviewDslDto.ShortReviewDsl;
+import io.oduck.api.domain.review.dto.ShortReviewReqDto;
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
-import io.oduck.api.domain.review.dto.ShortReviewResDto;
+import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReviewRes;
 import io.oduck.api.domain.review.entity.ShortReview;
 import io.oduck.api.domain.review.repository.ShortReviewRepository;
-import io.oduck.api.global.exception.BadRequestException;
+import io.oduck.api.global.common.OrderDirection;
+import io.oduck.api.global.common.SliceResponse;
 import io.oduck.api.global.exception.NotFoundException;
-import java.util.Optional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -22,12 +31,39 @@ import org.springframework.stereotype.Service;
 public class ShortReviewServiceImpl implements ShortReviewService{
 
     private final ShortReviewRepository shortReviewRepository;
+    private final MemberProfileRepository memberProfileRepository;
     private final MemberRepository memberRepository;
+
     private final AnimeRepository animeRepository;
 
     @Override
-    public ShortReviewResDto getShortReviews(Long anime) {
-        return null;
+    public SliceResponse<ShortReviewRes> getShortReviews(Long animeId, String cursor,ShortReviewReqDto.Sort sort, OrderDirection order, int size) {
+        Sort sortList = Sort.by(
+            Direction.fromString(order.getOrder()),
+            sort.getSort()
+        );
+
+        if(sort == ShortReviewReqDto.Sort.LIKE){
+            sortList = sortList.and(Sort.by(Direction.DESC, "createdAt"));
+        }else if(sort == ShortReviewReqDto.Sort.SCORE){
+            sortList = sortList.and(Sort.by(Direction.DESC, "createdAt"));
+        }
+
+        Slice<ShortReviewDsl> shortReviews = shortReviewRepository.selectShortReviews(
+            animeId,
+            cursor,
+            applyPageableForNonOffset(
+                size,
+                sortList
+            )
+        );
+
+        List<ShortReviewRes> res = shortReviews.getContent()
+                                       .stream()
+                                       .map(ShortReviewRes::of)
+                                       .toList();
+
+        return SliceResponse.of(shortReviews, res, sort.getSort());
     }
 
     @Override
@@ -46,55 +82,24 @@ public class ShortReviewServiceImpl implements ShortReviewService{
         shortReview.relateMember(getMember(memberId));
 
         ShortReview saveShortReview = shortReviewRepository.save(shortReview);
+        anime.increaseReviewCount();
 
-        Optional
-            .ofNullable(saveShortReview.getContent())
-            .ifPresent(
-                content ->{
-                    if(saveShortReview.getContent().equals(shortReview.getContent())){
-                        throw new BadRequestException("Failed to saveFail.");
-                    }
-                    //리뷰 작성 성공시 애니 리뷰카운트 증가
-                    anime.increaseReviewCount();
-                }
-            );
-        log.info("ShortReview Crated! {}", saveShortReview.getId());
+        //log.info("ShortReview Crated! {}", saveShortReview.getId());
     }
 
     @Override
     public void update(Long reviewId, PatchShortReviewReq req) {
         ShortReview findShortReview = getShortReview(reviewId);
-
-        Optional
-            .ofNullable(req.getContent())
-                .ifPresent(
-                    content ->{
-                        if(findShortReview.getContent().equals(req.getContent())){
-                            throw new BadRequestException("Same content as before.");
-                        }
-                        findShortReview.updateContent(req.getContent());
-                    }
-                );
-
-        Optional
-            .ofNullable(req.getContent())
-            .ifPresent(
-                hasSpoiler ->{
-                    if(findShortReview.isHasSpoiler() == req.isHasSpoiler()){
-                        throw new BadRequestException("Same spoiler as before.");
-                    }
-                    findShortReview.updateSpoiler(req.isHasSpoiler());
-                }
-            );
+        findShortReview.updateContent(req.getContent());
+        findShortReview.updateSpoiler(req.isHasSpoiler());
         shortReviewRepository.save(findShortReview);
     }
 
     private Member getMember(Long memberId){
-
         return memberRepository.findById(memberId)
-                   .orElseThrow(
-                       () -> new NotFoundException("Member")
-                   );
+                  .orElseThrow(
+                      () -> new NotFoundException("Member")
+                  );
     }
 
     private Anime getAnime(Long animeId){
