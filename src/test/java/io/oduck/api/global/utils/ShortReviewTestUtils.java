db6f634a5e0d78c60b7f9a6d389b88ba1f7c31c0
hanyMK <hany.mi.kim@gmail.com>
2023-10-27 17:44:41 +0900

    refactor: 짧은 리뷰 수정 및 생성 메소드 추가 #33

--- src/test/java/io/oduck/api/global/utils/ShortReviewTestUtils.java
+++ src/test/java/io/oduck/api/global/utils/ShortReviewTestUtils.java
@@ -1,7 +1,11 @@
 package io.oduck.api.global.utils;
 
+import static io.oduck.api.global.utils.AnimeTestUtils.createAnime;
+
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
+import io.oduck.api.domain.review.entity.ShortReview;
+import io.oduck.api.global.stub.MemberStub;
 
 public class ShortReviewTestUtils {
 
@@ -14,11 +18,11 @@ public class ShortReviewTestUtils {
 
     public static PatchShortReviewReq createPatchShortReview(){
         return new PatchShortReviewReq(
-            getAnimeId(), getName(), isHasSpoiler(),getContent()
+            getAnimeId(), getName(), isHasSpoiler(),updateContent()
         );
     }
 
-    public static String getName(){return "회원이름";}
+    public static String getName(){return "회원 이름";}
 
     public static Long getAnimeId(){
         return 1L;
@@ -28,5 +32,24 @@ public class ShortReviewTestUtils {
     }
     public static String getContent(){
         return "최고의 반전의 반전의 반전";
+    }
+
+    public static boolean updateHasSpoiler(){
+        return true;
+    }
+    public static String updateContent(){
+        return "멋진 결말 이였다 다시봐야지";
+    }
+
+
+
+    public static ShortReview createShortReview(){
+        return ShortReview
+                   .builder()
+                   .member(new MemberStub().getMember())
+                   .anime(createAnime())
+                   .content(getContent())
+                   .hasSpoiler(isHasSpoiler())
+                   .build();
     }
 }
