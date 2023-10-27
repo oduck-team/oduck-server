092a09049780991d5f46be89f6fb7e978e480b31
hanyMK <hany.mi.kim@gmail.com>
2023-10-27 17:46:46 +0900

    test: stub -> impl로 수정  #33

--- src/test/java/io/oduck/api/unit/attractionPoint/service/AttractionPointServiceTest.java
+++ src/test/java/io/oduck/api/unit/attractionPoint/service/AttractionPointServiceTest.java
@@ -3,18 +3,24 @@ package io.oduck.api.unit.attractionPoint.service;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import io.oduck.api.domain.attractionPoint.dto.AttractionPointResDto.IsAttractionPoint;
-import io.oduck.api.domain.attractionPoint.service.AttractionPointServiceStub;
+import io.oduck.api.domain.attractionPoint.repository.AttractionPointRepository;
+import io.oduck.api.domain.attractionPoint.service.AttractionPointServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class AttractionPointServiceTest {
     @InjectMocks
-    private AttractionPointServiceStub attractionPointServiceStub;
+    private AttractionPointServiceImpl attractionPointService;
+
+    @Mock
+    private AttractionPointRepository attractionPointRepository;
+
 
     @Nested
     @DisplayName("입덕 포인트 조회")
@@ -28,10 +34,10 @@ public class AttractionPointServiceTest {
             Long animeId = 1L;
 
             //when
-            IsAttractionPoint res = attractionPointServiceStub.isAttractionPoint(memberId, animeId);
+            IsAttractionPoint res = attractionPointService.isAttractionPoint(memberId, animeId);
 
             //then
-            assertDoesNotThrow(() -> attractionPointServiceStub.isAttractionPoint(memberId, animeId));
+            assertDoesNotThrow(() -> attractionPointService.isAttractionPoint(memberId, animeId));
         }
     }
 }
