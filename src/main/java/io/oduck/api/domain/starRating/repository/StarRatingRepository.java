f5b7817e255a7dea28d0837167884326f6b3cfe1
FaberJoo <sjdk0805@gmail.com>
2023-10-31 15:49:38 +0900

    feat: 회원 및 애니메 식별자로 조회 메소드 추가 #65

--- src/main/java/io/oduck/api/domain/starRating/repository/StarRatingRepository.java
+++ src/main/java/io/oduck/api/domain/starRating/repository/StarRatingRepository.java
@@ -1,8 +1,9 @@
 package io.oduck.api.domain.starRating.repository;
 
 import io.oduck.api.domain.starRating.entity.StarRating;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StarRatingRepository extends JpaRepository<StarRating, Long> {
-
+    Optional<StarRating> findByMemberIdAndAnimeId(Long memberId, Long animeId);
 }
