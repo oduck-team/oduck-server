8655309f0fea38028c134b6e36d50bc369f0f0d0
jaykayBaek <jaykaybaek@gmail.com>
2023-11-05 19:37:15 +0900

    feat: deletedAt이 null일 경우에 조회되는 메소드 추가 #63

--- src/main/java/io/oduck/api/domain/genre/repository/GenreRepository.java
+++ src/main/java/io/oduck/api/domain/genre/repository/GenreRepository.java
@@ -1,9 +1,15 @@
 package io.oduck.api.domain.genre.repository;
 
 import io.oduck.api.domain.genre.entity.Genre;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
   boolean existsByName(String name);
+
+  Optional<Genre> findByIdAndDeletedAtIsNull(Long genreId);
+
+  List<Genre> findAllByDeletedAtIsNull();
 }
--- src/main/java/io/oduck/api/domain/voiceActor/repository/VoiceActorRepository.java
+++ src/main/java/io/oduck/api/domain/voiceActor/repository/VoiceActorRepository.java
@@ -1,9 +1,15 @@
 package io.oduck.api.domain.voiceActor.repository;
 
 import io.oduck.api.domain.voiceActor.entity.VoiceActor;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoiceActorRepository extends JpaRepository<VoiceActor, Long> {
 
   boolean existsByName(String name);
+
+  List<VoiceActor> findAllByDeletedAtIsNull();
+
+  Optional<VoiceActor> findByIdAndDeletedAtIsNull(Long voiceActorId);
 }
