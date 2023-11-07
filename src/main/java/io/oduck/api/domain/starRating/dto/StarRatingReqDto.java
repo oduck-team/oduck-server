e35e7999eafbaa9307f1d8f1d8b43ac29e7c586c
FaberJoo <sjdk0805@gmail.com>
2023-11-07 23:32:13 +0900

    refactor: 별점 수정을 위한 Dto 추가 #90

--- src/main/java/io/oduck/api/domain/starRating/dto/StarRatingReqDto.java
+++ src/main/java/io/oduck/api/domain/starRating/dto/StarRatingReqDto.java
@@ -12,7 +12,7 @@ public class StarRatingReqDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class CreateReq {
+    public static class CreateAndPatchReq {
         @Min(value = 1, message = "score must be greater than or equal to 1")
         @Max(value = 10, message = "score must be less than or equal to 10")
         private int score;
