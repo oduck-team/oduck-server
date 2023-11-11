330cb6a8d47dee58621f39042d8de6d1626d2e77
hanyMK <hany.mi.kim@gmail.com>
2023-11-12 05:00:33 +0900

    style: 공백 삭제 #93

--- src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
@@ -35,7 +35,6 @@ public class ShortReviewServiceImpl implements ShortReviewService{
 
     private final ShortReviewRepository shortReviewRepository;
     private final MemberRepository memberRepository;
-
     private final AnimeRepository animeRepository;
 
 
