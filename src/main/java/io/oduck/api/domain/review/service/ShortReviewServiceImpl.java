4ca34a6dbae483be456e6a48fb4e52596a752ec6
hanyMK <hany.mi.kim@gmail.com>
2023-11-14 00:36:51 +0900

    refactor: 충돌 수정 #93

--- src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
+++ src/main/java/io/oduck/api/domain/review/service/ShortReviewServiceImpl.java
@@ -5,14 +5,11 @@ import static io.oduck.api.global.utils.PagingUtils.applyPageableForNonOffset;
 import io.oduck.api.domain.anime.entity.Anime;
 import io.oduck.api.domain.anime.repository.AnimeRepository;
 import io.oduck.api.domain.member.entity.Member;
-import io.oduck.api.domain.member.repository.MemberProfileRepository;
 import io.oduck.api.domain.member.repository.MemberRepository;
 import io.oduck.api.domain.review.dto.ShortReviewDslDto.ShortReviewDsl;
 import io.oduck.api.domain.review.dto.ShortReviewReqDto;
 import io.oduck.api.domain.review.dto.ShortReviewReqDto.ShortReviewReq;
 
-import io.oduck.api.domain.review.dto.ShortReviewReqDto.PatchShortReviewReq;
-import io.oduck.api.domain.review.dto.ShortReviewReqDto.PostShortReviewReq;
 import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReviewCountRes;
 import io.oduck.api.domain.review.dto.ShortReviewResDto.ShortReviewRes;
 import io.oduck.api.domain.review.entity.ShortReview;
@@ -101,16 +98,14 @@ public class ShortReviewServiceImpl implements ShortReviewService{
     }
 
     @Override
-    public void update(Long memberId, Long reviewId, ShortReviewReq req) {
     public ShortReviewCountRes getShortReviewCountByMemberId(Long memberId) {
         Long count = shortReviewRepository.countByMemberId(memberId);
         return ShortReviewCountRes.builder()
                    .count(count)
                    .build();
     }
-
     @Override
-    public void update(Long memberId, Long reviewId, PatchShortReviewReq req) {
+    public void update(Long memberId, Long reviewId, ShortReviewReq req) {
         ShortReview findShortReview = getShortReview(reviewId);
         Long findMemberId = findShortReview.getMember().getId();
         //리뷰 작성자 인지 확인
