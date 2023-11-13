40eb0146f8b76066d5502f827e123ef358ecc629
jaykayBaek <jaykaybaek@gmail.com>
2023-11-13 21:51:30 +0900

    feat: 단방향 연관 관계 추가 및 삭제 수정 #102

--- src/main/java/io/oduck/api/domain/genre/entity/Genre.java
+++ src/main/java/io/oduck/api/domain/genre/entity/Genre.java
@@ -1,12 +1,17 @@
 package io.oduck.api.domain.genre.entity;
 
+import io.oduck.api.domain.anime.entity.AnimeGenre;
 import io.oduck.api.global.audit.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,14 +27,19 @@ public class Genre extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false, length = 15, unique = true)
+  @Column(nullable = false, length = 15)
   private String name;
+
+  @OneToMany(mappedBy = "genre", cascade = CascadeType.PERSIST, orphanRemoval = true)
+  @Builder.Default
+  private List<AnimeGenre> animeGenres = new ArrayList<>();
 
   public void update(String name) {
     this.name = name;
   }
 
   public void delete() {
+    this.animeGenres.clear();
     this.deletedAt = LocalDateTime.now();
   }
 }
--- src/main/java/io/oduck/api/domain/voiceActor/entity/VoiceActor.java
+++ src/main/java/io/oduck/api/domain/voiceActor/entity/VoiceActor.java
@@ -1,12 +1,17 @@
 package io.oduck.api.domain.voiceActor.entity;
 
+import io.oduck.api.domain.anime.entity.AnimeVoiceActor;
 import io.oduck.api.global.audit.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,14 +27,19 @@ public class VoiceActor extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false, length = 50, unique = true)
+  @Column(nullable = false, length = 50)
   private String name;
+
+  @OneToMany(mappedBy = "voiceActor", cascade = CascadeType.PERSIST, orphanRemoval = true)
+  @Builder.Default
+  private List<AnimeVoiceActor> animeVoiceActors = new ArrayList<>();
 
   public void update(String name) {
     this.name = name;
   }
 
   public void delete() {
+    animeVoiceActors.clear();
     this.deletedAt = LocalDateTime.now();
   }
 }
