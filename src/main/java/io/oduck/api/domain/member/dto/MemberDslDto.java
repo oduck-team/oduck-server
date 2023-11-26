10b792ec465566278bba862b41e51cf2a9cac521
FaberJoo <sjdk0805@gmail.com>
2023-11-26 20:57:48 +0900

    refactor: 회원 DSL dto 수정 #130

--- src/main/java/io/oduck/api/domain/member/dto/MemberDslDto.java
+++ src/main/java/io/oduck/api/domain/member/dto/MemberDslDto.java
@@ -1,5 +1,6 @@
 package io.oduck.api.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.NoArgsConstructor;
 // TODO: VO로 변경 할지 논의 필요
 public class MemberDslDto {
     @Getter
+    @Builder
     @NoArgsConstructor
+    @AllArgsConstructor
     public static class ProfileWithoutActivity {
         private Long memberId;
         private String name;
@@ -15,29 +18,14 @@ public class MemberDslDto {
         private String thumbnail;
         private String backgroundImage;
         private Long point;
-
-        @Builder
-        public ProfileWithoutActivity(Long memberId, String name, String description, String thumbnail,
-            String backgroundImage, Long point) {
-            this.memberId = memberId;
-            this.name = name;
-            this.description = description;
-            this.thumbnail = thumbnail;
-            this.backgroundImage = backgroundImage;
-            this.point = point;
-        }
     }
 
     @Getter
+    @Builder
     @NoArgsConstructor
+    @AllArgsConstructor
     public static class MemberActivity {
         private Long reviews;
         private Long likes;
-
-        @Builder
-        public MemberActivity(Long reviews, Long likes) {
-            this.reviews = reviews;
-            this.likes = likes;
-        }
     }
 }
