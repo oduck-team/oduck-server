5ce39d178e90a853199c7565796639a287445b6e
jaykayBaek <jaykaybaek@gmail.com>
2023-11-29 23:47:31 +0900

    feat: 문의 요청, 응답 dto 구현 #135

---
+++ src/main/java/io/oduck/api/domain/inquiry/dto/InquiryReq.java
@@ -0,0 +1,28 @@
+package io.oduck.api.domain.inquiry.dto;
+
+import io.oduck.api.domain.inquiry.entity.InquiryType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+public class InquiryReq {
+    @Getter
+    @AllArgsConstructor
+    public static class PostReq {
+        @NotBlank
+        @Length(min = 1, max = 50,
+            message = "글자 수는 1~50을 허용합니다.")
+        private InquiryType type;
+
+        @NotBlank
+        @Length(min = 1, max = 50,
+            message = "글자 수는 1~50을 허용합니다.")
+        private String title;
+
+        @NotBlank
+        @Length(min = 1, max = 1000,
+            message = "글자 수는 1~1000를 허용합니다.")
+        private String content;
+    }
+}
