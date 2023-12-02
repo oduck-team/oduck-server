6e934e4aee6ca1f1fa653b1f2e7d852dc362233b
jaykayBaek <jaykaybaek@gmail.com>
2023-12-02 20:23:06 +0900

    cleanup: 기존 이름 제거 #138

--- src/main/java/io/oduck/api/domain/inquiry/repository/InquiryRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package io.oduck.api.domain.inquiry.repository;
-
-import io.oduck.api.domain.inquiry.dto.InquiryRes.MyInquiry;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface InquiryRepositoryCustom {
-    Page<MyInquiry> getAllByMemberId(Long memberId, Pageable pageable);
-}
