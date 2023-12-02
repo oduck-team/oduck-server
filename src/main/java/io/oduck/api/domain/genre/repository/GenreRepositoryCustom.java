3bb9d3a479f8c85b086689b0196cf57f3b2501ac
FaberJoo <sjdk0805@gmail.com>
2023-12-02 17:56:20 +0900

    feat: 장르 리스트 조회 repository 로직 추가 #140

--- src/main/java/io/oduck/api/domain/genre/repository/GenreRepositoryCustom.java
+++ src/main/java/io/oduck/api/domain/genre/repository/GenreRepositoryCustom.java
@@ -1,5 +1,10 @@
 package io.oduck.api.domain.genre.repository;
 
+import io.oduck.api.domain.genre.entity.Genre;
+import java.util.List;
+
 public interface GenreRepositoryCustom {
+
+    List<Genre> findAllByAnimeId(Long animeId);
     boolean existsByName(String name);
 }
