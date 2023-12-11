f41b98b3ab4d02b707867c7fecbc3cc68150fac2
hanyMK <hany.mi.kim@gmail.com>
2023-12-12 00:55:41 +0900

    cleanup: 사용하지 않는 import삭제 #145

--- src/main/java/io/oduck/api/domain/attractionPoint/service/AttractionPointServiceImpl.java
+++ src/main/java/io/oduck/api/domain/attractionPoint/service/AttractionPointServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Slf4j
